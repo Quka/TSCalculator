c08e0e48864403ef22631658d25c4b737f1581fd
Arlind Ukshini <quka93@gmail.com>
2018-10-02 14:40:35 +0200

    calculator done

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,41 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+function calculate() : void {
+    let n1 : number = parseInt((<HTMLInputElement> document.getElementById("n1")).value);
+    let n2 : number = parseInt((<HTMLInputElement> document.getElementById("n2")).value);
+
+    console.log("hej");
+
+    let calcType : string = (<HTMLInputElement> this).value;
+    let result : number;
+    let cType : string;
+
+    switch (calcType) {
+        case "+":
+            result = n1 + n2;
+            cType = "addition";
+            break;
+        case "-":
+            result = n1 - n2;
+            cType = "subtraction";
+            break;
+        case "*":
+            result = n1 * n2;
+            cType = "multiplication";
+            break;
+        case "/":
+            result = n1 / n2;
+            cType = "division";
+            break;
+    
+        default:
+            console.log("error use (+, -, *, /)");
+            break;
+    }
+
+    document.getElementById("result-span").innerText = cType + ": " + String(result);
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
+let mathBtns = document.getElementsByClassName("mathBtns");
+for (let i = 0; i < mathBtns.length; i++) {
+    let el = mathBtns[i];
+    el.addEventListener("click", calculate);
 }
-let user: Person = { firstName: "John", lastName: "Doe" };
-
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
